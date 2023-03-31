e94ebdcb59b0551281ae5bbebbb68d6a4503a8bc
Cifula <lidel95@gmail.com>
2023-03-31 17:48:27 +0900

    no message

---
+++ src/main/java/com/study/servlet/controller/Hello.java
@@ -0,0 +1,30 @@
+	package com.study.servlet.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Hello() {
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("최초 1회만 호출");
+	}
+
+	public void destroy() {
+		System.out.println("마지막 1회만 호출");
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("요청이 들어올 때마다 호출");
+	}
+
+}
